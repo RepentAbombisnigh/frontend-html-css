8ed1673f1b17cb6ad1ba5869500299f8b91f3b0e
repentabombisnigh <ywaa1206@gmail.com>
2023-11-07 17:58:24 +0900

    1107

--- JS/1102/z_review/script.js
+++ JS/1102/z_review/script.js
@@ -1,5 +1,12 @@
 // prompt 창으로 입력받은 값은 기본적으로 string 지정
 
-let r;
-const PI = 3.14;
-console.log(PI * r * r);
+let r = 10;
+let PI = 3.14;
+let cir = PI * r * r;
+console.log(`넓이 : {cir}`);
+
+
+let inch
+inch = promt('cm로 변환할 inch를 입력해주세요.');
+cm = inch * 2.54
+document.write(`{inch}inch는 {cm}cm입니다.`);
