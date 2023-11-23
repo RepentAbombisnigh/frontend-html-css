cc394f0276d2844a5e28e1c9973e30e3a7f1d056
repentabombisnigh <ywaa1206@gmail.com>
2023-11-23 17:50:11 +0900

    1123

---
+++ JS/1123/z_practice/main.js
@@ -0,0 +1,32 @@
+//? 아이템들을 저장할 배열(items)
+
+//? 새 아이템을 생성하는 함수(createItem)
+  //: 입력 필드의 DOM 요소를 읽어오기
+  //: 입력 필드의 값을 공백을 제허가고 가져오기(trim())
+  //: 새 아이템이 비어있지 않은 경우
+    // 조건문을 사용해 기존 아이템 배열에 새 아이템을 추가 (전개 연산자 사용 filter())
+    // 입력 필드 초기화
+    // 아이템 목록을 다시 렌더링(renderItems)
+
+//? 아이템을 삭제하는 함수(deleteItem)
+  // 특정 인덱스의 아이템을 제외한 새로운 배열을 생성(filter())
+    //: 선택된 요소의 인덱스 번호를 매개변수로 전달받음
+  // 아이템 목록을 다시 렌더링(renderItems)
+
+//? 아이템 목록을 렌더링하는 함수(renderItems)
+  // 아이템 목록을 담은 DOM 요소를 읽어오기
+  // 목록 초기화(innerHTML를 빈 문자열로 할당)
+
+  // 아이템 배열을 순회하며 목록 생성(forEach)
+  //: item과 index 번호를 매개변수로 전달
+    // 각 아이템을 위한 li요소 생성
+    // const li - document.createElement('li');
+    // li 요소에 아이템 텍스트 추가
+    // li.textContent = item;
+    // li 요소 클릭 시 해당 아이템 삭제 함수 호출
+    // (호출 시 index 번호를 인수로 전달)
+    // 생성한 li 요소를 목록에 추가
+    // itemList.AppenChild(li);
+
+// 문서가 로드 되었을 때 아이템 목록을 처음으로 렌더링
+document.addEventListener('DOMContentLoaded', renderItems);
